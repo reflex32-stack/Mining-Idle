7ad2821c246c8f68e54c8ce764b7d630de0daad5
reflex32-stack <kreimeldaniel4@gmail.com>
2026-09-26 22:08:31 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = "idle-miner-v16";
+const CACHE = "idle-miner-v17";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
